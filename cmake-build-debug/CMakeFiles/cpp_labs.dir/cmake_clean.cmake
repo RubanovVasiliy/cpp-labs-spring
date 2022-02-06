172feb1b3039cc1def744e578be30b174cd1542a
Vasiliy <rubanovas2005@gmail.com>
2022-02-06 16:14:19 +0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/cpp_labs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_labs.dir/main.cpp.o"
+  "cpp_labs"
+  "cpp_labs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_labs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
