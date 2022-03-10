f50bd84f3fa2ad5bdecef059f9c53caad540418f
Rubanov Vasiliy <rubanovas2005@gmail.com>
2022-03-10 23:03:13 +0700

    Created test file

---
+++ test/libcsc/libcsc/list/list.cpp
@@ -0,0 +1,10 @@
+#include <list/LinkedList.h>
+#include <gtest/gtest.h>
+
+
+TEST(LinkedList, 1) {
+
+    const auto value = 10;
+    const auto expected_value = 10;
+    ASSERT_EQ(value, expected_value);
+}
