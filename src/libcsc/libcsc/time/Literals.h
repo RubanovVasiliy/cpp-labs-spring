883bc634825d2a0dbc104ab19879ed817c67619e
Rubanov Vasiliy <rubanovas2005@gmail.com>
2022-03-13 23:35:52 +0700

    Changed operators argument type from uint_64 to unsigned long long

--- src/libcsc/libcsc/time/Literals.h
+++ src/libcsc/libcsc/time/Literals.h
@@ -1,18 +1,18 @@
 #include <time/MyTime.h>
  namespace literals {
-     my_time::Time operator ""_s(uint64_t time) {
+     my_time::Time operator ""_s(unsigned long long time) {
         return my_time::Time(static_cast<time_t>(time));
     }
 
-     my_time::Time operator ""_m(uint64_t time) {
+     my_time::Time operator ""_m(unsigned long long time) {
         return my_time::Time(static_cast<time_t>(time) * my_time::seconds_in_minute);
     }
 
-     my_time::Time operator ""_h(uint64_t time) {
+     my_time::Time operator ""_h(unsigned long long time) {
         return my_time::Time(static_cast<time_t>(time) * my_time::seconds_in_hour);
     }
 
-     my_time::Time operator ""_d(uint64_t time) {
+     my_time::Time operator ""_d(unsigned long long time) {
         return my_time::Time(static_cast<time_t>(time) * my_time::seconds_in_day);
     }
 }
